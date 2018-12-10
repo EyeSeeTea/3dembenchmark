2ed5bfaf72f6a14c358c8efe5552fbc2b2b944c9
Ray Dario <raydario@gmail.com>
2018-12-10 22:07:45 +0000

    New 3DEM Benchmark Design Fixes

--- web/urls.py
+++ web/urls.py
@@ -3,6 +3,7 @@ from web import views_home
 
 urlpatterns = [
     url(r'^$', views_home.home, name='home'),
-    url(r'^page/$', views_home.page, name='page'),
-    url(r'^ctf/$', views_home.ctf, name='ctf'),
+    url(r'^particlepicking/$', views_home.particlepicking, name='particlepicking'),
+    url(r'^ctfdetection/$', views_home.ctfdetection, name='ctfdetection'),
+    url(r'^resolutionenhancement/$', views_home.resolutionenhancement, name='resolutionenhancement'),
 ]
--- web/views_home.py
+++ web/views_home.py
@@ -4,8 +4,11 @@ from django.shortcuts import render_to_response
 def home(request):
     return render_to_response('home/index.html')
 
-def page(request):
-    return render_to_response('page/index.html')
+def particlepicking(request):
+    return render_to_response('particlepicking/index.html')
 
-def ctf(request):
-    return render_to_response('ctf/index.html')
+def ctfdetection(request):
+    return render_to_response('ctfdetection/index.html')
+
+def resolutionenhancement(request):
+    return render_to_response('resolutionenhancement/index.html')    
