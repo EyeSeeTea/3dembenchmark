ef4b484fdc8ec1fe5a9ef40c6f2496e6bf1c2fac
Vito <vito@iMac-de-Vito.local>
2019-04-05 07:58:34 +0100

    Added results pages & fixes

--- web/views_home.py
+++ web/views_home.py
@@ -16,5 +16,92 @@ def ctfdetection(request):
 def ctfdetectionresults(request):
     return render_to_response('ctfdetection/results/index.html')
 
+def ctfdetectiondatasets(request):
+    return render_to_response('ctfdetection/datasets/index.html')
+
 def resolutionenhancement(request):
     return render_to_response('resolutionenhancement/index.html')
+
+def ctfdetectionresults206(request):
+        return render_to_response('ctfdetection/results/206.html')
+
+def ctfdetectionresults207(request):
+        return render_to_response('ctfdetection/results/207.html')
+
+def ctfdetectionresults250(request):
+        return render_to_response('ctfdetection/results/250.html')
+
+def ctfdetectionresults256(request):
+        return render_to_response('ctfdetection/results/256.html')
+
+def ctfdetectionresults257(request):
+        return render_to_response('ctfdetection/results/257.html')
+
+def ctfdetectionresults282(request):
+        return render_to_response('ctfdetection/results/282.html')
+
+def ctfdetectionresults287(request):
+        return render_to_response('ctfdetection/results/287.html')
+
+def ctfdetectionresults291(request):
+        return render_to_response('ctfdetection/results/291.html')
+
+def ctfdetectionresults292(request):
+        return render_to_response('ctfdetection/results/292.html')
+
+def ctfdetectionresults296(request):
+        return render_to_response('ctfdetection/results/296.html')
+
+def ctfdetectionresults297(request):
+        return render_to_response('ctfdetection/results/297.html')
+
+def ctfdetectionresults298(request):
+        return render_to_response('ctfdetection/results/298.html')
+
+def ctfdetectionresults299(request):
+        return render_to_response('ctfdetection/results/299.html')
+
+def ctfdetectionresults300(request):
+        return render_to_response('ctfdetection/results/300.html')
+
+def ctfdetectionresults301(request):
+        return render_to_response('ctfdetection/results/301.html')
+
+def ctfdetectionresults303(request):
+        return render_to_response('ctfdetection/results/303.html')
+
+def ctfdetectionresults304(request):
+        return render_to_response('ctfdetection/results/304.html')
+
+def ctfdetectionresults310(request):
+        return render_to_response('ctfdetection/results/310.html')
+
+def ctfdetectionresults312(request):
+        return render_to_response('ctfdetection/results/312.html')
+
+def ctfdetectionresults314(request):
+        return render_to_response('ctfdetection/results/314.html')
+
+def ctfdetectionresults318(request):
+        return render_to_response('ctfdetection/results/318.html')
+
+def ctfdetectionresults336(request):
+        return render_to_response('ctfdetection/results/336.html')
+
+def ctfdetectionresults337(request):
+        return render_to_response('ctfdetection/results/337.html')
+
+def ctfdetectionresults339(request):
+        return render_to_response('ctfdetection/results/339.html')
+
+def ctfdetectionresults340(request):
+        return render_to_response('ctfdetection/results/340.html')
+
+def ctfdetectionresults341(request):
+        return render_to_response('ctfdetection/results/341.html')
+
+def ctfdetectionresults342(request):
+        return render_to_response('ctfdetection/results/342.html')        
+
+def ctfdetectionresults344(request):
+        return render_to_response('ctfdetection/results/344.html')
